d00eb56ded6577e1bdf6237a01bab96c2d18a9fc
MangoMunky <77705856+MangoMunky@users.noreply.github.com>
2024-06-15 15:32:59 -0400

    Major changes
    Added a typewriter animation. Made navbar sticky. added autoscroll positions in navbar. added footer and contact page

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-function toTop(){
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+//https://github.com/TomIsLoading/typing-animation
+
+//code for autotype
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  const phrases = ["adaptive", "eager to learn", "fast"];
+  const el = document.getElementById("typewriter");
+
+  let sleepTime = 100;
+
+  let curPhraseIndex = 0;
+
+  const writeLoop = async () => {
+    while (true) {
+      let curWord = phrases[curPhraseIndex];
+
+      for (let i = 0; i < curWord.length; i++) {
+        el.innerText = curWord.substring(0, i + 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 10);
+
+      for (let i = curWord.length; i > 0; i--) {
+        el.innerText = curWord.substring(0, i - 1);
+        await sleep(sleepTime);
+      }
+
+      await sleep(sleepTime * 5);
+
+      if (curPhraseIndex === phrases.length - 1) {
+        curPhraseIndex = 0;
+      } else {
+        curPhraseIndex++;
+      }
+    }
+  };
+
+  writeLoop();
